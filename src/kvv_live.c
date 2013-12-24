fe350b5703dded3e082885009fdeaad34521a790
Luchs <Luchs@euirc.eu>
2013-12-24 13:07:47 +0100

    Enable scrolling
    For test purposes, the five new departures are just copies...

--- src/kvv_live.c
+++ src/kvv_live.c
@@ -2,25 +2,32 @@
 #include <departure.h>
 
 static Window *window;
+static ScrollLayer *scroll_layer;
 
-#define DEPARTURE_LINES 5
+#define DEPARTURE_LINES 10
 static struct DepartureLine *lines[DEPARTURE_LINES];
 static struct Departure departures[DEPARTURE_LINES];
 
 static void window_load(Window *window) {
 	Layer *window_layer = window_get_root_layer(window);
 	GRect bounds = layer_get_bounds(window_layer);
+	scroll_layer = scroll_layer_create(bounds);
+	scroll_layer_set_click_config_onto_window(scroll_layer, window);
+	layer_add_child(window_layer, scroll_layer_get_layer(scroll_layer));
 
 	// Render lines.
 	const int line_height = DEPARTURE_HEIGHT + 1;
-	for (unsigned int i = 0; i < DEPARTURE_LINES; i++) {
+	unsigned int i;
+	for (i = 0; i < DEPARTURE_LINES; i++) {
 		int ypos = 3 + line_height * i;
 		lines[i] = departure_line_create(&departures[i], (GRect) { .origin = { 0, ypos }, .size = { bounds.size.w, 20 } });
-		layer_add_child(window_layer, lines[i]->layer);
+		scroll_layer_add_child(scroll_layer, lines[i]->layer);
 	}
+	scroll_layer_set_content_size(scroll_layer, (GSize) { .h = 5 + line_height * i, .w = bounds.size.w });
 }
 
 static void window_unload(Window *window) {
+	scroll_layer_destroy(scroll_layer);
 	for (unsigned int i = 0; i < DEPARTURE_LINES; i++) {
 		departure_line_destroy(lines[i]);
 	}
@@ -43,6 +50,22 @@ static void init(void) {
 	departures[4].route       = "S4";
 	departures[4].destination = "Tullastrasse";
 	departures[4].time        = "13:15";
+
+	departures[5].route       = "S4";
+	departures[5].destination = "Tullastrasse";
+	departures[5].time        = "3min";
+	departures[6].route       = "S41";
+	departures[6].destination = "Freudenstadt Hbf";
+	departures[6].time        = "12:25";
+	departures[7].route       = "S41";
+	departures[7].destination = "Tullastrasse";
+	departures[7].time        = "12:35";
+	departures[8].route       = "S4";
+	departures[8].destination = "Achern";
+	departures[8].time        = "12:45";
+	departures[9].route       = "S4";
+	departures[9].destination = "Tullastrasse";
+	departures[9].time        = "13:15";
 
 	window = window_create();
 	window_set_window_handlers(window, (WindowHandlers) {
