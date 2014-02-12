77dfe891fb6171d298b59d142f08dff46a3f7198
Luchs <Luchs@euirc.eu>
2014-02-12 10:14:04 +0000

    Use Base64 encoding to fix äöüß issues

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -9,17 +9,17 @@ Pebble.addEventListener('ready', function(e) {
 
 // Called when the user wants to configure the app.
 Pebble.addEventListener('showConfiguration', function(e) {
-  console.log("Opening configuration");
-  Pebble.openURL('http://kvv-live.lwrl.de/#' + encodeURIComponent(localStorage['favorites']));
+  console.log("Opening configuration", localStorage['config']);
+  Pebble.openURL('http://kvv-live.lwrl.de/#' + localStorage['config']);
 });
 
 // Called when the user is done configuring the app.
 Pebble.addEventListener('webviewclosed', function(e) {
-  var resp = decodeURIComponent(e.response);
+  var resp = e.response;
   // Null when pressing "Cancel".
   if (resp) {
-    favorites = JSON.parse(resp).favorites;
-    localStorage['favorites'] = resp;
+    favorites = JSON.parse(b64_to_utf8(resp)).favorites;
+    localStorage['config'] = resp;
     console.log("Received " + favorites.length + " favorites");
     transferFavorites(favorites);
   }
@@ -142,3 +142,86 @@ function first(array) {
 function rest(array) {
   return array.slice(1);
 }
+
+/* UTF-8 <-> Base64 encoding. */
+function b64_to_utf8(str) {
+  return decodeURIComponent(escape(atob(str)));
+}
+
+/* atob/btoa polyfill, taken from https://github.com/inexorabletash/polyfill/blob/4551a44c206e43fc012c507ec2fb9d30fe9367ca/web.js#L618-L693 */
+(function (global) {
+  var B64_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+  global.atob = global.atob || function (input) {
+    input = String(input);
+    var position = 0,
+        output = [],
+        buffer = 0, bits = 0, n;
+
+    input = input.replace(/\s/g, '');
+    if ((input.length % 4) === 0) { input = input.replace(/=+$/, ''); }
+    if ((input.length % 4) === 1) { throw Error("InvalidCharacterError"); }
+    if (/[^+/0-9A-Za-z]/.test(input)) { throw Error("InvalidCharacterError"); }
+
+    while (position < input.length) {
+      n = B64_ALPHABET.indexOf(input.charAt(position));
+      buffer = (buffer << 6) | n;
+      bits += 6;
+
+      if (bits === 24) {
+        output.push(String.fromCharCode((buffer >> 16) & 0xFF));
+        output.push(String.fromCharCode((buffer >>  8) & 0xFF));
+        output.push(String.fromCharCode(buffer & 0xFF));
+        bits = 0;
+        buffer = 0;
+      }
+      position += 1;
+    }
+
+    if (bits === 12) {
+      buffer = buffer >> 4;
+      output.push(String.fromCharCode(buffer & 0xFF));
+    } else if (bits === 18) {
+      buffer = buffer >> 2;
+      output.push(String.fromCharCode((buffer >> 8) & 0xFF));
+      output.push(String.fromCharCode(buffer & 0xFF));
+    }
+
+    return output.join('');
+  };
+
+  global.btoa = global.btoa || function (input) {
+    input = String(input);
+    var position = 0,
+        out = [],
+        o1, o2, o3,
+        e1, e2, e3, e4;
+
+    if (/[^\x00-\xFF]/.test(input)) { throw Error("InvalidCharacterError"); }
+
+    while (position < input.length) {
+      o1 = input.charCodeAt(position++);
+      o2 = input.charCodeAt(position++);
+      o3 = input.charCodeAt(position++);
+
+      // 111111 112222 222233 333333
+      e1 = o1 >> 2;
+      e2 = ((o1 & 0x3) << 4) | (o2 >> 4);
+      e3 = ((o2 & 0xf) << 2) | (o3 >> 6);
+      e4 = o3 & 0x3f;
+
+      if (position === input.length + 2) {
+        e3 = 64; e4 = 64;
+      }
+      else if (position === input.length + 1) {
+        e4 = 64;
+      }
+
+      out.push(B64_ALPHABET.charAt(e1),
+               B64_ALPHABET.charAt(e2),
+               B64_ALPHABET.charAt(e3),
+               B64_ALPHABET.charAt(e4));
+    }
+
+    return out.join('');
+  };
+}(this));
