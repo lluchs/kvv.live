c1ef1f5a7b8001e89327dd687a61b913e809f62c
Luchs <Luchs@euirc.eu>
2014-11-09 20:18:51 +0100

    Change the stop_window to use MenuLayer

--- src/stops_window.c
+++ src/stops_window.c
@@ -9,25 +9,27 @@
 #include "stops.h"
 
 static Window *window;
-static SimpleMenuLayer *menu;
+static MenuLayer *menu;
 
 static struct stops *stops;
-static SimpleMenuItem *menu_entries;
-static SimpleMenuSection menu_section;
+
+static void init_menu_layer();
 
 static void window_load(Window *window) {
 	Layer *window_layer = window_get_root_layer(window);
 	GRect bounds = layer_get_bounds(window_layer);
 
-	menu = simple_menu_layer_create(bounds, window, &menu_section, 1, NULL);
-	layer_add_child(window_layer, simple_menu_layer_get_layer(menu));
+	menu = menu_layer_create(bounds);
+	init_menu_layer();
+
+	layer_add_child(window_layer, menu_layer_get_layer(menu));
 }
 
 static void window_unload(Window *window) {
-	simple_menu_layer_destroy(menu);
+	menu_layer_destroy(menu);
 }
 
-static void show_departures(int index, void *context) {
+static void show_departures(int index) {
 	char *stop_id = stops->ids[index];
 	APP_LOG(APP_LOG_LEVEL_DEBUG, "Showing departures for station %s.", stop_id);
 	departures_window_show(stop_id);
@@ -35,15 +37,6 @@ static void show_departures(int index, void *context) {
 
 void stops_window_init() {
 	stops = read_stops();
-
-	menu_entries = (SimpleMenuItem*)calloc(stops->num, sizeof(SimpleMenuItem));
-	menu_section.num_items = stops->num;
-	menu_section.items = menu_entries;
-
-	for (unsigned int i = 0; i < menu_section.num_items; i++) {
-		menu_entries[i].title = stops->names[i];
-		menu_entries[i].callback = show_departures;
-	}
 
 	window = window_create();
 	window_set_window_handlers(window, (WindowHandlers) {
@@ -56,13 +49,82 @@ void stops_window_init() {
 
 void stops_window_deinit() {
 	stops_destroy(stops);
-	free(menu_entries);
 
 	window_destroy(window);
 }
 
 void stops_window_reload() {
-	stops_window_deinit();
-	window_stack_pop_all(true);
-	stops_window_init();
+	stops_destroy(stops);
+	stops = read_stops();
+	menu_layer_reload_data(menu);
+}
+
+/* Menu callbacks */
+
+// A callback is used to specify the amount of sections of menu items
+// With this, you can dynamically add and remove sections
+static uint16_t menu_get_num_sections_callback(MenuLayer *menu_layer, void *data) {
+  return 1;
+}
+
+// Each section has a number of items;  we use a callback to specify this
+// You can also dynamically add and remove items using this
+static uint16_t menu_get_num_rows_callback(MenuLayer *menu_layer, uint16_t section_index, void *data) {
+	switch (section_index) {
+		case 0: // favorites
+			return stops->num;
+
+		default:
+			return 0;
+	}
+}
+
+// A callback is used to specify the height of the section header
+static int16_t menu_get_header_height_callback(MenuLayer *menu_layer, uint16_t section_index, void *data) {
+	// This is a define provided in pebble.h that you may use for the default height
+	return MENU_CELL_BASIC_HEADER_HEIGHT;
+}
+
+// Here we draw what each header is
+static void menu_draw_header_callback(GContext* ctx, const Layer *cell_layer, uint16_t section_index, void *data) {
+	switch (section_index) {
+		case 0:
+			// Draw title text in the section header
+			menu_cell_basic_header_draw(ctx, cell_layer, "Favorites");
+			break;
+	}
+}
+
+// This is the menu item draw callback where you specify what each item should look like
+static void menu_draw_row_callback(GContext* ctx, const Layer *cell_layer, MenuIndex *cell_index, void *data) {
+	// Determine which section we're going to draw in
+	switch (cell_index->section) {
+		case 0: // favorites
+			menu_cell_basic_draw(ctx, cell_layer, stops->names[cell_index->row], NULL, NULL);
+			break;
+	}
+}
+
+// Here we capture when a user selects a menu item
+void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *data) {
+	// Use the row to specify which item will receive the select action
+	switch (cell_index->section) {
+		case 0: // favorites
+			show_departures(cell_index->row);
+			break;
+	}
+
+}
+
+static void init_menu_layer() {
+	// Set all the callbacks for the menu layer
+	menu_layer_set_callbacks(menu, NULL, (MenuLayerCallbacks){
+		.get_num_sections = menu_get_num_sections_callback,
+		.get_num_rows = menu_get_num_rows_callback,
+		.get_header_height = menu_get_header_height_callback,
+		.draw_header = menu_draw_header_callback,
+		.draw_row = menu_draw_row_callback,
+		.select_click = menu_select_callback,
+	});
+	menu_layer_set_click_config_onto_window(menu, window);
 }
